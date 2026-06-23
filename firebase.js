2e26340ef1928dff63a05b3817c32ab527e1adda
Martin123215 <reyescristopher018@gmail.com>
2026-06-23 10:03:29 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,31 +1,23 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-import {
-  getDatabase
-} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js";
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDwo5QyyVgbwrQX10u8Fls59imdrNqEESM",
+    authDomain: "inven-d458a.firebaseapp.com",
+    projectId: "inven-d458a",
+    storageBucket: "inven-d458a.firebasestorage.app",
+    messagingSenderId: "420195585893",
+    appId: "1:420195585893:web:6ffc43afe908239c291b8c",
+    measurementId: "G-L5Y3834DEG"
+  };
 
-const firebaseConfig = {
-
-  apiKey: "TU_APIKEY",
-
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-
-  databaseURL:
-  "https://TU_PROYECTO-default-rtdb.firebaseio.com",
-
-  projectId: "TU_PROYECTO",
-
-  storageBucket:
-  "TU_PROYECTO.firebasestorage.app",
-
-  messagingSenderId: "XXXX",
-
-  appId: "XXXX"
-
-};
-
-const app =
-initializeApp(firebaseConfig);
-
-export const db =
-getDatabase(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
